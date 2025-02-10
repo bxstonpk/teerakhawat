1a94e531a0801a17373f6946f4f624cff92d6cf6
bxstonpk <s6419410031@sau.ac.th>
2025-02-10 17:00:50 +0700

    get all /

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        publicPath: '/',
-      },
-    },
-  },
   server: {
     historyApiFallback: true,
   },
